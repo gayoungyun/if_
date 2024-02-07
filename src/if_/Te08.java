3ab6d9b64b776e10a7014f76a734ca1c40dd9106
yungayeong <gyy94@naver.com>
2024-02-07 17:47:32 +0900

    수정

--- src/if_/Te08.java
+++ src/if_/Te08.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Te08 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int mon ;
+		int dal = 1326;
+	
+		
+		System.out.println("환전단위");
+		dal = input.nextInt();
+		
+		
 		
 	}
 
